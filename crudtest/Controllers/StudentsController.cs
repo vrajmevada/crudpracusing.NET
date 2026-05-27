51028c234bebf1cb337bd53a24ecca442fe1990e
vrajmevada <81295552+vrajmevada@users.noreply.github.com>
2026-05-27 11:29:00 +0530

    Initial commit

---
+++ crudtest/Controllers/StudentsController.cs
@@ -0,0 +1,151 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using crudtest.Models;
+using crudtest.Data;
+
+namespace crudtest.Controllers;
+public class StudentsController : Controller
+{
+    private readonly SchoolContext _context;
+
+    public StudentsController(SchoolContext context)
+    {
+        _context = context;
+    }
+
+    // GET: STUDENTS
+    public async Task<IActionResult> Index()    
+    {
+        return View(await _context.Students.ToListAsync());
+    }
+
+    // GET: STUDENTS/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var student = await _context.Students
+            .FirstOrDefaultAsync(m => m.ID == id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        return View(student);
+    }
+
+    // GET: STUDENTS/Create
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: STUDENTS/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,EnrollmentDate,Enrollments")] Student student)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(student);
+    }
+
+    // GET: STUDENTS/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var student = await _context.Students.FindAsync(id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+        return View(student);
+    }
+
+    // POST: STUDENTS/Edit/5
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int? id, [Bind("ID,LastName,FirstMidName,EnrollmentDate,Enrollments")] Student student)
+    {
+        if (id != student.ID)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(student);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(student.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(student);
+    }
+
+    // GET: STUDENTS/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var student = await _context.Students
+            .FirstOrDefaultAsync(m => m.ID == id);
+        if (student == null)
+        {
+            return NotFound();
+        }
+
+        return View(student);
+    }
+
+    // POST: STUDENTS/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? id)
+    {
+        var student = await _context.Students.FindAsync(id);
+        if (student != null)
+        {
+            _context.Students.Remove(student);
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool StudentExists(int? id)
+    {
+        return _context.Students.Any(e => e.ID == id);
+    }
+}
---
+++ crudtest/Program.cs
@@ -0,0 +1,51 @@
+using crudtest.Data;
+using crudtest.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<crudtest.Data.SchoolContext>(options =>
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+// Seed database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<SchoolContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
